9047e41e05935dd324cf1b0a1fbfe7161d6d45b2
Wes Koerber <wes@weskoerber.com>
2023-11-02 04:22:07 -0400

    feat: add functions for slice comparison
    - allow slice comparison with both other slices and char arrays without
      allocating memory

--- include/slice.h
+++ include/slice.h
@@ -8,3 +8,6 @@ typedef struct {
 } slice;
 
 char *slice_to_string(const slice *const slice);
+
+long slice_cmp(const slice *s1, const slice *s2);
+long slice_cmp_str(const slice *s, const char *str);
--- src/slice.c
+++ src/slice.c
@@ -1,5 +1,6 @@
 #include "slice.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -11,4 +12,22 @@ char *slice_to_string(const slice *const slice) {
   str[slice->length] = 0;
 
   return str;
+}
+
+long slice_cmp(const slice *s1, const slice *s2) {
+  size_t i = 0;
+  for (; *(char *)(s1->start + i) == *(char *)(s2->start + i) &&
+         i < s1->length - 1;
+       i++)
+    ;
+
+  return *(unsigned char *)(s1->start + i) - *(unsigned char *)(s2->start + i);
+}
+
+long slice_cmp_str(const slice *s, const char *str) {
+  size_t i = 0;
+  for (; *(char *)(s->start + i) == *(str + i) && i < s->length - 1; i++)
+    ;
+
+  return *(unsigned char *)(s->start + i) - *(unsigned char *)(str + i);
 }
