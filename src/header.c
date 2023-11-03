181b34659670ca2eb591fdf45e02dd87026169d2
Wes Koerber <wes@weskoerber.com>
2023-11-03 00:18:51 -0400

    chore: optimize strings

--- src/header.c
+++ src/header.c
@@ -8,71 +8,50 @@
 #include <string.h>
 
 const char *resolution_str[] = {
-  [b1] = "1-bit",
-  [b4] = "4-bit",
-  [b8] = "8-bit",
-  [b16] = "16-bit",
-  [b32] = "32-bit",
+  [b1] = "1",
+  [b4] = "4",
+  [b8] = "8",
+  [b16] = "16",
+  [b32] = "32",
 };
 
 const char *byteorder_str[] = {
-  [little_endian] = "little_endian",
-  [big_endian] = "big_endian",
+  [little_endian] = "I",
+  [big_endian] = "M",
 };
 
 const char *layout_str[] = {
-  [bil] = "bil",
-  [bip] = "bip",
-  [bsq] = "bsq",
+  [bil] = "BIL",
+  [bip] = "BIP",
+  [bsq] = "BSQ",
 };
 
 const char *pixeltype_str[] = {
-  [signedint] = "signedint",
-  [unsignedint] = "unsignedint",
+  [signedint] = "SIGNEDINT",
+  [unsignedint] = "UNSIGNEDINT",
 };
+
+const char header_str_nrows[] = "NROWS";
+const char header_str_ncols[] = "NCOLS";
+const char header_str_nbands[] = "NBANDS";
+const char header_str_nbits[] = "NBITS";
+const char header_str_byteorder[] = "BYTEORDER";
+const char header_str_layout[] = "LAYOUT";
+const char header_str_skipbytes[] = "SKIPBYTES";
+const char header_str_ulxmap[] = "ULXMAP";
+const char header_str_ulymap[] = "ULYMAP";
+const char header_str_xdim[] = "XDIM";
+const char header_str_ydim[] = "YDIM";
+const char header_str_bandrowbytes[] = "BANDROWBYTES";
+const char header_str_totalrowbytes[] = "TOTALROWBYTES";
+const char header_str_bandgapbytes[] = "BANDGAPBYTES";
+const char header_str_pixeltype[] = "PIXELTYPE";
+const char header_str_nodata[] = "NODATA";
 
 typedef struct {
   const char *const key;
   bool (*const parse)(const slice *, header *const);
 } header_proc_map;
-
-typedef struct {
-  const char *const nrows;
-  const char *const ncols;
-  const char *const nbands;
-  const char *const nbits;
-  const char *const byteorder;
-  const char *const layout;
-  const char *const skipbytes;
-  const char *const ulxmap;
-  const char *const ulymap;
-  const char *const xdim;
-  const char *const ydim;
-  const char *const bandrowbytes;
-  const char *const totalrowbytes;
-  const char *const bandgapbytes;
-  const char *const pixeltype;
-  const char *const nodata;
-} header_fields;
-
-const header_fields header_fields_ = {
-  .nrows = "NROWS",
-  .ncols = "NCOLS",
-  .nbands = "NBANDS",
-  .nbits = "NBITS",
-  .byteorder = "BYTEORDER",
-  .layout = "LAYOUT",
-  .skipbytes = "SKIPBYTES",
-  .ulxmap = "ULXMAP",
-  .ulymap = "ULYMAP",
-  .xdim = "XDIM",
-  .ydim = "YDIM",
-  .bandrowbytes = "BANDROWBYTES",
-  .totalrowbytes = "TOTALROWBYTES",
-  .bandgapbytes = "BANDGAPBYTES",
-  .pixeltype = "PIXELTYPE",
-  .nodata = "NODATA",
-};
 
 const header *const header_parse(const char *data);
 
@@ -98,22 +77,22 @@ bool header_parse_pixeltype(const slice *data, header *header);
 bool header_parse_nodata(const slice *data, header *header);
 
 const header_proc_map header_proc_map_[] = {
-  {header_fields_.nrows,         header_parse_nrows        },
-  {header_fields_.ncols,         header_parse_ncols        },
-  {header_fields_.nbands,        header_parse_nbands       },
-  {header_fields_.nbits,         header_parse_nbits        },
-  {header_fields_.byteorder,     header_parse_byteorder    },
-  {header_fields_.layout,        header_parse_layout       },
-  {header_fields_.skipbytes,     header_parse_skipbytes    },
-  {header_fields_.ulxmap,        header_parse_ulxmap       },
-  {header_fields_.ulymap,        header_parse_ulymap       },
-  {header_fields_.xdim,          header_parse_xdim         },
-  {header_fields_.ydim,          header_parse_ydim         },
-  {header_fields_.bandrowbytes,  header_parse_bandrowbytes },
-  {header_fields_.totalrowbytes, header_parse_totalrowbytes},
-  {header_fields_.bandgapbytes,  header_parse_bandgapbytes },
-  {header_fields_.pixeltype,     header_parse_pixeltype    },
-  {header_fields_.nodata,        header_parse_nodata       },
+  {header_str_nrows,         header_parse_nrows        },
+  {header_str_ncols,         header_parse_ncols        },
+  {header_str_nbands,        header_parse_nbands       },
+  {header_str_nbits,         header_parse_nbits        },
+  {header_str_byteorder,     header_parse_byteorder    },
+  {header_str_layout,        header_parse_layout       },
+  {header_str_skipbytes,     header_parse_skipbytes    },
+  {header_str_ulxmap,        header_parse_ulxmap       },
+  {header_str_ulymap,        header_parse_ulymap       },
+  {header_str_xdim,          header_parse_xdim         },
+  {header_str_ydim,          header_parse_ydim         },
+  {header_str_bandrowbytes,  header_parse_bandrowbytes },
+  {header_str_totalrowbytes, header_parse_totalrowbytes},
+  {header_str_bandgapbytes,  header_parse_bandgapbytes },
+  {header_str_pixeltype,     header_parse_pixeltype    },
+  {header_str_nodata,        header_parse_nodata       },
 };
 const size_t num_header_procs =
   sizeof(header_proc_map_) / sizeof(header_proc_map_[0]);
@@ -208,19 +187,19 @@ bool header_parse_nbands(const slice *data, header *header) {
 bool header_parse_nbits(const slice *data, header *header) {
   bool status = false;
 
-  if (slice_cmp_str(data, "1")) {
+  if (slice_cmp_str(data, resolution_str[b1])) {
     header->nbits = b1;
     status = true;
-  } else if (slice_cmp_str(data, "4")) {
+  } else if (slice_cmp_str(data, resolution_str[b4])) {
     header->nbits = b4;
     status = true;
-  } else if (slice_cmp_str(data, "8")) {
+  } else if (slice_cmp_str(data, resolution_str[b8])) {
     header->nbits = b8;
     status = true;
-  } else if (slice_cmp_str(data, "16")) {
+  } else if (slice_cmp_str(data, resolution_str[b16])) {
     header->nbits = b16;
     status = true;
-  } else if (slice_cmp_str(data, "32")) {
+  } else if (slice_cmp_str(data, resolution_str[b32])) {
     header->nbits = b32;
     status = true;
   }
@@ -231,10 +210,10 @@ bool header_parse_nbits(const slice *data, header *header) {
 bool header_parse_byteorder(const slice *data, header *header) {
   bool status = false;
 
-  if (slice_cmp_str(data, "I") == 0) {
+  if (slice_cmp_str(data, byteorder_str[little_endian]) == 0) {
     header->byteorder = little_endian;
     status = true;
-  } else if (slice_cmp_str(data, "M") == 0) {
+  } else if (slice_cmp_str(data, byteorder_str[big_endian]) == 0) {
     header->byteorder = big_endian;
     status = true;
   }
@@ -245,13 +224,13 @@ bool header_parse_byteorder(const slice *data, header *header) {
 bool header_parse_layout(const slice *data, header *header) {
   bool status = false;
 
-  if (slice_cmp_str(data, "BIL")) {
+  if (slice_cmp_str(data, layout_str[bil])) {
     header->layout = bil;
     status = true;
-  } else if (slice_cmp_str(data, "BIP")) {
+  } else if (slice_cmp_str(data, layout_str[bip])) {
     header->layout = bip;
     status = true;
-  } else if (slice_cmp_str(data, "BSQ")) {
+  } else if (slice_cmp_str(data, layout_str[bsq])) {
     header->layout = bsq;
     status = true;
   }
@@ -310,10 +289,10 @@ bool header_parse_bandgapbytes(const slice *data, header *header) {
 bool header_parse_pixeltype(const slice *data, header *header) {
   bool status = false;
 
-  if (slice_cmp_str(data, "SIGNEDINT")) {
+  if (slice_cmp_str(data, pixeltype_str[signedint])) {
     header->pixeltype = signedint;
     status = true;
-  } else if (slice_cmp_str(data, "UNSIGNEDINT")) {
+  } else if (slice_cmp_str(data, pixeltype_str[unsignedint])) {
     header->pixeltype = unsignedint;
     status = true;
   }
